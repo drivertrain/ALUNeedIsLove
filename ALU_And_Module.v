5a1ce55d7abc0aa26b07b3ea0e6bdd99045242f3
mikeyesudas12345 <mikeyesudas@gmail.com>
2019-11-16 14:11:34 -0600

    And Module with slight modification

--- ALU_And_Module.v
+++ ALU_And_Module.v
@@ -5,9 +5,9 @@
 
 module AndModule(bigMuxIn3, inA, inB);
 
-    output [7:0] bigMuxIn3;
-    input [7:0] inA;
-    input [7:0] inB;
+    output [15:0] bigMuxIn3;
+    input [15:0] inA;
+    input [15:0] inB;
 
     and(bigMuxIn3[0], inA[0], inB[0]);
     and(bigMuxIn3[1], inA[1], inB[1]);
@@ -17,6 +17,14 @@ module AndModule(bigMuxIn3, inA, inB);
     and(bigMuxIn3[5], inA[5], inB[5]);
     and(bigMuxIn3[6], inA[6], inB[6]);
     and(bigMuxIn3[7], inA[7], inB[7]);
+    and(bigMuxIn3[8], inA[8], inB[8]);
+    and(bigMuxIn3[9], inA[9], inB[9]);
+    and(bigMuxIn3[10], inA[10], inB[10]);
+    and(bigMuxIn3[11], inA[11], inB[11]);
+    and(bigMuxIn3[12], inA[12], inB[12]);
+    and(bigMuxIn3[13], inA[13], inB[13]);
+    and(bigMuxIn3[14], inA[14], inB[14]);
+    and(bigMuxIn3[15], inA[15], inB[15]);
 
 endmodule // AndModule
 
