eaadc23786f109f4b509157aa64a45db325eda27
Max Nguyen <prodeeprod@gmail.com>
2019-11-18 04:42:04 -0600

    Fixed Addersub code

--- Verilog/AdderSubtractor.v
+++ Verilog/AdderSubtractor.v
@@ -1,78 +1,54 @@
-module ha(a,b, sum, cout); //sub module for Half adder
-input a,b;
-output sum,cout;
-xor m1(sum,a,b);
-and m2(cout,a,b);
+module Half_Adder (input A, B, output c_out, sum);
+	xor G1 (sum, A, B);
+	and G2 (c_out, A, B);
 endmodule
 
-module fa(a,b,cin, sum, cout); //full-adder
-input a,b,cin;
-output sum,cout;
-
-wire s1,c1,c2;
-ha m1(a,b,s1,c1);
-ha m2(cin,w1,sum,c2);
-or m3(cout,c1,c2);
+module Full_Adder (input A, B, c_in, output c_out, sum);
+	wire w1, w2, w3; // w1 = c_out, w2 = sum
+	Half_Adder M1 (A, B, w1, w2);
+	Half_Adder M2 (w2, c_in, w3, sum);
+	or (c_out, w1, w3);
 endmodule
 
-module fa_structural(a,b,cin,sum,cout); //done structurally
-input [7:0] a,b;
-input cin;
-output [7:0] sum;
-output cout;
+module Adder_Subtractor(input [7:0] A, B, input c_in, output c_out, output [7:0] sum); //done structurally
 
-wire c1,c2,c3,c4,c5,c6,c7;
-fa m1(a[0], b[0], cin, sum[0], c1);
-fa m2(a[1], b[1], c1, sum[1], c2);
-fa m3(a[2], b[2], c2, sum[2], c3);
-fa m4(a[3], b[3], c3, sum[3], c4);
-fa m5(a[4], b[4], c4, sum[4], c5);
-fa m6(a[5], b[5], c5, sum[5], c6);
-fa m7(a[6], b[6], c6, sum[6], c7);
-fa m8(a[7], b[7], c7, sum[7], cout);
-endmodule
+wire [7:0] f;
+wire c_in1, c_in2, c_in3, c_in4, c_in5, c_in6, cin_7;
 
-module add_sub(a, b, selector, sum, cout); //adder and subtractor
-input [7:0] a,b;
-input selector; //0 for add, 1 for sub
-output [7:0] sum;
-output cout;
+	xor G1(f[0], c_in, B[0]);
+	xor G2(f[1], c_in, B[1]);
+	xor G3(f[2], c_in, B[2]);
+	xor G4(f[3], c_in, B[3]);
+	xor G5(f[4], c_in, B[4]);
+	xor G6(f[5], c_in, B[5]);
+	xor G7(f[6], c_in, B[6]);
+	xor G8(f[7], c_in, B[7]);
 
-wire x0, x1, x2, x3, x4, x5, x6, x7;
-wire c1,c2,c3,c4,c5,c6,c7;
-
-xor switch0(x0, selector, b[0]);
-xor switch1(x1, selector, b[1]);
-xor switch2(x2, selector, b[2]);
-xor switch3(x3, selector, b[3]);
-xor switch4(x4, selector, b[4]);
-xor switch5(x5, selector, b[5]);
-xor switch6(x6, selector, b[6]);
-xor switch7(x7, selector, b[7]);
-
-fa m1(a[0], b[0], selector, sum[0], c1);
-fa m2(a[1], b[1], c1, sum[1], c2);
-fa m3(a[2], b[2], c2, sum[2], c3);
-fa m4(a[3], b[3], c3, sum[3], c4);
-fa m5(a[4], b[4], c4, sum[4], c5);
-fa m6(a[5], b[5], c5, sum[5], c6);
-fa m7(a[6], b[6], c6, sum[6], c7);
-fa m8(a[7], b[7], c7, sum[7], cout);
+	Full_Adder M0 (.A(A[0]), .B(f[0]), .c_in(c_in), .c_out(c_in1), .sum(sum[0]));
+	Full_Adder M1 (.A(A[1]), .B(f[1]), .c_in(c_in1), .c_out(c_in2), .sum(sum[1]));
+	Full_Adder M2 (.A(A[2]), .B(f[2]), .c_in(c_in2), .c_out(c_in3), .sum(sum[2]));
+	Full_Adder M3 (.A(A[3]), .B(f[3]), .c_in(c_in3), .c_out(c_in4), .sum(sum[3]));
+	Full_Adder M4 (.A(A[4]), .B(f[4]), .c_in(c_in4), .c_out(c_in5), .sum(sum[4]));
+	Full_Adder M5 (.A(A[5]), .B(f[5]), .c_in(c_in5), .c_out(c_in6), .sum(sum[5]));
+	Full_Adder M6 (.A(A[6]), .B(f[6]), .c_in(c_in6), .c_out(c_in7), .sum(sum[6]));
+	Full_Adder M7 (.A(A[7]), .B(f[7]), .c_in(c_in7), .c_out(c_out), .sum(sum[7]));
 endmodule
 
 
-module testbench(); //tester not working; need second eyes
-  reg [7:0] inputA = 8'b00111111;
-    reg [7:0] inputB = 8'b11110010;
-    reg select = 0;
-    wire [7:0] summer;
-    wire c1;
-add_sub m1(inputB, inputA, select, summer, c1);
-initial begin
- $display ("|##|A|B|Select|Sum|Cout|");
-  $display ("|==+=+=+=+=+==|"); 
-$display ("|%2d|%1d|%1d|%1d|%1d|",inputA,inputB,select,summer,c1);
-#5
-$finish;
-end
+module testBench(); //tester working
+	reg [7:0] A, B; 
+    reg c_in;
+    wire [7:0] sum;
+    wire c_out;
+	
+	Adder_Subtractor testing(A, B, c_in, c_out, sum);
+	initial begin
+	
+		$monitor ("A = %1d, B = %1d, c_in = %1d, c_out = %B, sum = %d", A, B, c_in, c_out, sum);
+
+		A = 13; B = 7; c_in = 0; #100
+		A = 64; B = 32; c_in = 1; #100
+
+		$finish;
+	end
 endmodule
