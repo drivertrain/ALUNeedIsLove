21645f47a04f4390b8c558d453c21f3a95dd15d5
mikeyesudas12345 <mikeyesudas@gmail.com>
2019-11-22 00:18:35 -0600

    Only DFlipFlop module

--- Verilog/ALU_Accumulator.v
+++ Verilog/ALU_Accumulator.v
@@ -2,3 +2,17 @@
 // Put in full adder module 
 // TODO: Basically translate what you did in the lab into Verilog
 
+module DFlipFlop (clk, in, out, clr, pre);
+    parameter n = 16; 
+    
+    input clk; 
+    input [n-1:0] in;
+    
+    output [n-1:0] out;
+    
+    reg [n-1:0] out;
+
+    always @ (posedge clk)
+    out = in;
+
+endmodule // DFlipFlop
